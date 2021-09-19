7c5d554eeb0bfe322fbd6dabef35f9b650d3f8d9
danielecollins <57637750+danielecollins@users.noreply.github.com>
2021-09-18 23:41:32 -0600

    Made a buch of css, html, and js changes

---
+++ script.js
@@ -0,0 +1,16 @@
+let options = {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    time: "long"
+};
+
+let timeOptions = {
+    hour12: false,
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit" 
+};
+
+document.getElementById("date-last-modified").textContent = new Date(document.lastModified).toLocaleDateString("en-US", options);
+document.getElementById("time-last-modified").textContent = new Date(document.lastModified).toLocaleTimeString("en-US", timeOptions);
