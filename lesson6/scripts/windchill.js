67a7a63b9a2f0ef043f26ebbfb80121eb4ea6818
danielecollins <57637750+danielecollins@users.noreply.github.com>
2021-10-24 13:22:39 -0600

    Added the wind chill calculation

---
+++ lesson6/scripts/windchill.js
@@ -0,0 +1,7 @@
+let t = document.getElementById('high').innerHTML;
+let s = document.getElementById('wind-speed').innerHTML;
+
+if (t <= 50 && s > 4.8)
+    document.getElementById('wind-chill').innerHTML = Math.round(35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16));
+else
+    document.getElementById('wind-chill').innerHTML = "N/A";
