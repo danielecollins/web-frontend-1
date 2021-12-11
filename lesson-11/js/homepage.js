401ae7cc65c699fee9f5b06a3d855552f7f0be09
danielecollins <57637750+danielecollins@users.noreply.github.com>
2021-12-10 19:03:23 -0700

    ussed the towndata json file for the homepage

--- lesson-11/js/homepage.js
+++ lesson-11/js/homepage.js
@@ -1,40 +1,43 @@
-const citiesJSON = 'cities.json';
+const towndataJSON = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
-fetch(citiesJSON)
+fetch(towndataJSON)
     .then(function(response) {
         return response.json();
     })
     .then(function (jsonObject) {
         console.table(jsonObject);
-        const cities = jsonObject['cities'];
-        for (let i = 0; i < cities.length; i++) {
-            let card = document.createElement('section');
-            let text = document.createElement('div');
-            let townName = document.createElement('h2');
-            let motto = document.createElement('p');
-            let yearFounded = document.createElement('p');
-            let population = document.createElement('p');
-            let annualRainfall = document.createElement('p');
-            let image = document.createElement('img');
+        const towns = jsonObject['towns'];
+        for (let i = 0; i < towns.length; i++) {
+            if (towns[i].name === "Soda Springs" || towns[i].name === "Fish Haven" || towns[i].name === "Preston")
+            {
+                let card = document.createElement('section');
+                let text = document.createElement('div');
+                let townName = document.createElement('h2');
+                let motto = document.createElement('p');
+                let yearFounded = document.createElement('p');
+                let population = document.createElement('p');
+                let annualRainfall = document.createElement('p');
+                let image = document.createElement('img');
 
-            townName.textContent = cities[i].name;
-            motto.textContent = cities[i].motto;
-            motto.setAttribute('class', 'motto');
-            yearFounded.textContent = `Year Founded: ${cities[i].yearfounded}`;
-            population.textContent = `Population: ${cities[i].population}`;
-            annualRainfall.textContent = `Annual Rainfall: ${cities[i].annualrainfall}`;
-            
-            image.setAttribute('src', cities[i].imagepath);
-            image.setAttribute('alt', "picture of the outdoors");
+                townName.textContent = towns[i].name;
+                motto.textContent = towns[i].motto;
+                motto.setAttribute('class', 'motto');
+                yearFounded.textContent = `Year Founded: ${towns[i].yearFounded}`;
+                population.textContent = `Population: ${towns[i].currentPopulation}`;
+                annualRainfall.textContent = `Annual Rainfall: ${towns[i].averageRainfall}`;
+                
+                image.setAttribute('src', 'images/' + towns[i].photo);
+                image.setAttribute('alt', "picture of the outdoors");
 
-            text.appendChild(townName);
-            text.appendChild(motto);
-            text.appendChild(yearFounded);
-            text.appendChild(population);
-            text.appendChild(annualRainfall);
-            card.appendChild(text);
-            card.appendChild(image);
+                text.appendChild(townName);
+                text.appendChild(motto);
+                text.appendChild(yearFounded);
+                text.appendChild(population);
+                text.appendChild(annualRainfall);
+                card.appendChild(text);
+                card.appendChild(image);
 
-            document.querySelector('div.cards').appendChild(card);
+                document.querySelector('div.cards').appendChild(card);
+            } 
         }
     })
