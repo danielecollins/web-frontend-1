d6041c4ad3d78a7979f94593285bd061cf69ae4f
danielecollins <57637750+danielecollins@users.noreply.github.com>
2021-12-12 03:32:27 -0700

    finished the our partners page

---
+++ boldedgeadv/js/lazyloader.js
@@ -0,0 +1,30 @@
+
+const sources = document.querySelectorAll("source");
+
+function preloadImage(src) {
+    const srcset = src.getAttribute("data-src");
+    if(!srcset)
+        return;
+    src.srcset = srcset;
+    console.log("load")
+}
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 0px 0px"
+};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting)
+            return;
+        else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    })
+}, imgOptions);
+
+sources.forEach(source => {
+    imgObserver.observe(source);
+});
